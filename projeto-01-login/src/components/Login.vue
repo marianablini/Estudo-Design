22b9fd3af0556d257ce4da6e5abd78e62b7a9a26
marianablini <mariana_blini@hotmail.com>
2020-06-13 15:45:15 -0300

    layout

--- projeto-01-login/src/components/Login.vue
+++ projeto-01-login/src/components/Login.vue
@@ -1,32 +1,37 @@
 <template>
-  <v-container>
-    <v-card class="mx-auto">
+  <v-container class="container-login">
+    <v-card class="mx-auto pa-5" cols="12" sm="12" md="6" lg="6">
       <v-card-text>
-        <div>Word of the Day</div>
-        <p class="display-1 text--primary">
-          be•nev•o•lent
-        </p>
-        <p>adjective</p>
-        <div class="text--primary">
-          well meaning and kindly.<br>
-          "a benevolent smile"
-        </div>
+        <v-text-field v-model="email" label="Email" required></v-text-field>
+        <v-text-field v-model="password" label="Senha" required></v-text-field>
       </v-card-text>
-      <v-card-actions>
-        <v-btn text color="deep-purple accent-4">
-          Learn More
-        </v-btn>
-      </v-card-actions>
+      <v-row>
+        <v-col cols="12" class="pa-0 text-center">
+          <v-btn class="white--text" color="light-green"> Entrar </v-btn>
+        </v-col>
+        <v-col class="text-center" cols="12">
+           Não tem uma conta?<v-btn @click="$emit('cadastro', true)" text color="blue">Cadastre aqui!</v-btn>
+        </v-col>
+      </v-row>
     </v-card>
   </v-container>
 </template>
 
 <script>
   export default {
-    name: 'login',
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
   }
 </script>
 
-<style>
-  
+<style scoped>
+.container-login {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+}
 </style>
